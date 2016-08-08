b874650d624046b118d5bedaaadf4d955f75daf1
Anikait Singh <singh.anikait@gmail.com>
2016-08-08 07:55:16 -0400

    Add files via upload

---
+++ problem%20bank/Diff.java
@@ -0,0 +1,13 @@
+package week4;
+
+public class Diff {
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4};
+		int x = evenOddDiff(arr); 
+		System.out.println(x); //should print out the value of two
+	}
+	public static int evenOddDiff(int[] arr){
+		//complete this method
+		return 0;
+	}
+}
